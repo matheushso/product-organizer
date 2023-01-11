5103d9dfcd57746935062322c31b6d80adca483d
matheushso <matheushso99@hotmail.com>
2023-01-11 19:41:03 -0300

    first commit

---
+++ src/main/java/com/product/organizer/ProductOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.product.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrganizerApplication.class, args);
+	}
+
+}
